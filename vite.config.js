6e788dd2a718813254fd3e3007e84b5621f7955e
Rashad Edwards <rashadedwards@macbook.local>
2024-12-09 20:39:55 -0500

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://zenquotes.io", // The API's base URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Strip '/api' prefix
-      },
+      "/api": "https://zenquotes.io", // Replace with your API's base URL
     },
   },
 });
