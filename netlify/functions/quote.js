4d863f933c0e6748c4089403baecffe0fcb943db
Rashad Edwards <rashadedwards@macbook.local>
2024-12-09 21:47:18 -0500

    deployment

--- netlify/functions/quote.js
+++ netlify/functions/quote.js
@@ -3,8 +3,13 @@ import axios from "axios";
 export const handler = async (event, context) => {
   try {
     const response = await axios.get("https://zenquotes.io/api/quotes");
+
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow all origins or restrict to specific domains
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(response.data),
     };
   } catch (error) {
