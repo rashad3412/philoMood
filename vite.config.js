806ada6dc77361476da94877b100c17aec26c3f9
Rashad Edwards <rashadedwards@macbook.local>
2024-12-09 21:30:06 -0500

    errors

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://zenquotes.io", // Replace with your API's base URL
-    },
-  },
 });
